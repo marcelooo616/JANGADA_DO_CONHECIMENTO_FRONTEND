a74d0d1295e825bcff40ca063dd05db14500e05b
Marcelo Santos <marcelooo2014@hotmail.com>
2025-08-15 08:32:42 -0300

    fix - corrigindo key

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,7 +15,7 @@ import CertificationBanner from '../components/CertificationBanner';
 
 
 const API_URL = 'http://137.131.212.103:3000/api';
-const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+const NEWS_API_KEY = "1e8f2b8f70f849aeb216d4f97f814822";
 
 // Função que busca os artigos da NOSSA API. Ela fica fora do componente.
 const fetchArticles = async ({ queryKey }) => {
