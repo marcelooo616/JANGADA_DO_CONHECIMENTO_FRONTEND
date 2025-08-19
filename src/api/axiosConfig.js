52dc947664ee557296d680b04220065fd5b75c85
Marcelo Santos <marcelooo2014@hotmail.com>
2025-08-19 07:50:20 -0300

    fix - adicionando conexão com o db

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,9 +2,10 @@
 import axios from 'axios';
 
 // Cria uma instância do axios com a configuração base
-//baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+///baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // 
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
